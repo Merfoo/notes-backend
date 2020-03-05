4ef3506b2ef6148afda491520f67039c4f7e139a
Fauzi Kliman <fauzimerfoo@gmail.com>
2020-03-04 20:20:56 -0800

    Add filter, skip, first, nd orderBy for notes query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,30 @@
 
-function notes(parent, args, context) {
-    return context.prisma.notes();
+async function notes(parent, { filter, skip, first, orderBy }, context) {
+    const where = filter ? {
+        OR: [
+            { title_contains: filter },
+            { body_contains: filter }
+        ]
+    } : {};
+
+    const notes = await context.prisma.notes({
+        where,
+        skip,
+        first,
+        orderBy
+    });
+
+    const count = await context.prisma
+        .notesConnection({
+            where
+        })
+        .aggregate()
+        .count();
+
+    return {
+        notes,
+        count
+    };
 }
 
 module.exports = {
